0af236a15b28c6ec4403fc1e73b13481d7fc8c72
Nirup Kushalnagar <nk3393@nyu.edu>
2024-11-17 18:43:02 -0600

    f

--- app/src/components/LinkedinVerify.js
+++ app/src/components/LinkedinVerify.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./LinkedinVerify.css"; // Import the CSS file
 
 const LinkedInPopup = ({ isOpen, onClose, onVerificationComplete }) => {
