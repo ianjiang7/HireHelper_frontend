4e7799eb519cad5f397bf914680e1c5c0397c4c1
nkushalnagar <nk3393@nyu.edu>
2024-12-10 17:13:34 -0500

    Update Signup.js

--- app/src/components/jsfiles/Signup.js
+++ app/src/components/jsfiles/Signup.js
@@ -219,7 +219,7 @@ function Signup() {
       // Save role in localStorage
       localStorage.setItem("userRole", signupData.role);
       await new Promise((resolve) => setTimeout(resolve, 3000));
-      navigate('/profile-setup', { replace: true, state: { skipAuth: true } });
+      navigate('/profile-setup');
       // Navigate to appropriate page
     } catch (err) {
       console.error("Confirmation error:", err);
